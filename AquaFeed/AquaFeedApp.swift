9417a64b3fe533e72228dc545b512354046cecda
Rodriguez <mar9688@weslaco.cs.utexas.edu>
2026-06-17 10:22:38 -0500

    Initial commit

---
+++ AquaFeed/AquaFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaFeedApp.swift
+//  AquaFeed
+//
+//  Created by Rodriguez, Melody A on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct AquaFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
